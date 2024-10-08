ec8b31131183d85076760a15f5743864bab00409
“Nitin <nitinkumar7h@gmail.com>
2024-10-08 08:59:13 +0530

    Scanner Button

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,21 +3,31 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center ">
-      <Link href="/loginDashboard">
+    <div className="flex flex-col gap-8 min-h-screen items-center justify-center ">
+      <div className="flex gap-4">
+        <Link href="/loginDashboard">
+          <button
+            type="button"
+            className="text-white bg-purple-800 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            Login
+          </button>
+        </Link>
+        <Link href="/signupDashboard">
+          <button
+            type="button"
+            className="text-white bg-purple-800 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            Signup
+          </button>
+        </Link>
+      </div>
+      <Link href="/qrscanner">
         <button
           type="button"
-          className="text-white bg-purple-800 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          className="text-white bg-green-700 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
         >
-          Login
-        </button>
-      </Link>
-      <Link href="/signupDashboard">
-        <button
-          type="button"
-          className="text-white bg-purple-800 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
-        >
-          Signup
+          Scanner
         </button>
       </Link>
     </div>
--- src/components/qr/QrScanner.jsx
+++ src/components/qr/QrScanner.jsx
@@ -5,40 +5,47 @@ import "react-toastify/dist/ReactToastify.css";
 export default function QrScanner() {
   const result = (e) => {
     // console.log("Scan result: ", e);
-    const mainData = e[0].rawValue;
-    // console.log("mainData : ", mainData);
-    const jsonData = JSON.parse(mainData);
+    try {
+      const mainData = e[0].rawValue;
+      console.log("mainData : ", mainData);
+      const jsonData = JSON.parse(mainData);
 
-    const sendData = async () => {
-      if (jsonData) {
-        const response = await fetch("api/student/attendance", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+      const sendData = async () => {
+        if (jsonData) {
+          const response = await fetch("api/student/attendance", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
 
-          body: JSON.stringify(jsonData),
-        });
+            body: JSON.stringify(jsonData),
+          });
 
-        const dataFromResponse = await response.json();
-        // console.log("dataFromResponse : ", dataFromResponse);
-        if (dataFromResponse.data === false) {
-          toast.error(dataFromResponse.message);
-          return setTimeout(() => {
+          const dataFromResponse = await response.json();
+          // console.log("dataFromResponse : ", dataFromResponse);
+          if (dataFromResponse.data === false) {
+            toast.error(dataFromResponse.message);
+            return setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+          }
+          toast.success(
+            `${dataFromResponse.message} with Student Id : ${jsonData.studentId}`
+          );
+          // alert(dataFromResponse.message);
+          setTimeout(() => {
             window.location.reload();
           }, 2000);
         }
-        toast.success(
-          `${dataFromResponse.message} with Student Id : ${jsonData.studentId}`
-        );
-        // alert(dataFromResponse.message);
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      }
-    };
+      };
 
-    sendData();
+      sendData();
+    } catch (error) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+      toast.error("Please Scan only Generated QR ");
+    }
   };
 
   return (
